4e2d9c2ba2465a316a134f4446aa53a8f2273e9b
jovanemartins-sis <jovanedez@gmail.com>
2026-08-16 17:30:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,319 +11,68 @@ const app = express();
 app.use(express.json({ limit: '5mb' }));
 app.use(cors());
 
-// Configurações do Emitente (Dados de Ibitinga - SP)
+// --- CONFIGURAÇÃO ---
 const EMITENTE = {
     cnpj: "66304541000111",
     xNome: "66.304.541 ELAINE CRISTINA DE CAMARGO DE SOUZA",
-    ie: "344275522110",
-    enderEmit: {
-        xLgr: "Rua Exemplo",
-        nro: "123",
-        xBairro: "Centro",
-        cMun: "3519608", // Código IBGE de Ibitinga - SP
-        xMun: "Ibitinga",
-        uf: "SP",
-        cep: "14940000"
-    }
+    ie: "344275522110"
 };
 
-// Função para carregar o certificado digital das variáveis de ambiente do Render
+// --- FUNÇÕES AUXILIARES ---
 function carregarCertificado() {
-    try {
-        if (!process.env.CERT_BASE64) {
-            console.log("Aviso: CERT_BASE64 não configurado nas variáveis de ambiente.");
-            return null;
-        }
-        const certBuffer = Buffer.from(process.env.CERT_BASE64.replace(/\s/g, ''), 'base64');
-        const certPath = path.join(__dirname, 'certificado.pfx');
-        fs.writeFileSync(certPath, certBuffer);
-        console.log("Certificado digital carregado e gravado com sucesso!");
-        return certPath;
-    } catch (erro) {
-        console.error("Erro crítico ao carregar o certificado:", erro.message);
-        return null;
-    }
+    if (!process.env.CERT_BASE64) return null;
+    const certBuffer = Buffer.from(process.env.CERT_BASE64.replace(/\s/g, ''), 'base64');
+    const certPath = path.join(__dirname, 'certificado.pfx');
+    fs.writeFileSync(certPath, certBuffer);
+    return certPath;
 }
-
 carregarCertificado();
 
-// Rota de Status
-app.get('/', (req, res) => {
-    res.json({ 
-        status: "API Fiscal NFC-e Rodando com Assinatura e Transmissão SEFAZ-SP!",
-        emitente: EMITENTE.xNome,
-        cnpj: EMITENTE.cnpj
-    });
-});
-
-// Função para formatar a data no padrão exigido pela SEFAZ
-function obterDataSefaz() {
-    const agora = new Date();
-    const ano = agora.getFullYear();
-    const mes = String(agora.getMonth() + 1).padStart(2, '0');
-    const dia = String(agora.getDate()).padStart(2, '0');
-    const hora = String(agora.getHours()).padStart(2, '0');
-    const min = String(agora.getMinutes()).padStart(2, '0');
-    const seg = String(agora.getSeconds()).padStart(2, '0');
-    return `${ano}-${mes}-${dia}T${hora}:${min}:${seg}-03:00`;
-}
-
-// Função para calcular o Dígito Verificador (DV) da Chave de Acesso (Módulo 11)
 function calcularDV(chave43) {
-    let soma = 0;
-    let peso = 2;
+    let soma = 0, peso = 2;
     for (let i = chave43.length - 1; i >= 0; i--) {
         soma += parseInt(chave43.charAt(i)) * peso;
-        peso++;
-        if (peso > 9) peso = 2;
+        peso = (peso === 9) ? 2 : peso + 1;
     }
     let resto = soma % 11;
-    let dv = 11 - resto;
-    if (dv >= 10) dv = 0;
-    return String(dv);
+    return (resto < 2) ? "0" : String(11 - resto);
 }
 
-// Rota 1: Gerar o XML e Assinar Digitalmente
+// --- ROTAS ---
+app.get('/', (req, res) => res.json({ status: "API Ativa" }));
+
 app.post('/emitir-nfce', (req, res) => {
     try {
         const nNF = Math.floor(Math.random() * 999999) + 1;
-        const cUF = "35"; // SP
-        const aamm = "2608"; // Ano/Mês atual
-        const cnpj = EMITENTE.cnpj;
-        const mod = "65"; // NFC-e
-        const serie = "001";
         const nNFStr = String(nNF).padStart(9, '0');
-        const tpEmis = "1"; // Emissão normal
-        const cNF = "00000001"; // Código numérico aleatório/sequencial
-        
-        const chave43 = cUF + aamm + cnpj + mod + serie + nNFStr + tpEmis + cNF;
-        const cDV = calcularDV(chave43);
-        const chNFe = chave43 + cDV;
+        const chBase = "3526086630454100011165001" + nNFStr + "100000001";
+        const chNFe = chBase + calcularDV(chBase);
 
-        const tpAmb = process.env.AMBIENTE_PRODUCAO === 'true' ? "1" : "2"; 
-        const dhEmiStr = obterDataSefaz();
+        // XML Minificado para evitar falha de schema
+        let xmlNFe = `<NFe xmlns="http://www.portalfiscal.inf.br/nfe"><infNFe Id="NFe${chNFe}" versao="4.00"><ide><cUF>35</cUF><cNF>00000001</cNF><natOp>VENDAS</natOp><mod>65</mod><serie>1</serie><nNF>${nNF}</nNF><dhEmi>2026-08-16T17:20:00-03:00</dhEmi><tpNF>1</tpNF><idDest>1</idDest><cMunFG>3519608</cMunFG><tpImp>4</tpImp><tpEmis>1</tpEmis><cDV>${chNFe.slice(-1)}</cDV><tpAmb>2</tpAmb><finNFe>1</finNFe><indFinal>1</indFinal><indPres>1</indPres><procEmi>0</procEmi><verProc>1.0</verProc></ide><emit><CNPJ>${EMITENTE.cnpj}</CNPJ><xNome>${EMITENTE.xNome}</xNome><enderEmit><xLgr>Rua Exemplo</xLgr><nro>123</nro><xBairro>Centro</xBairro><cMun>3519608</cMun><xMun>Ibitinga</xMun><UF>SP</UF><CEP>14940000</CEP><cPais>1058</cPais><xPais>BRASIL</xPais></enderEmit><IE>${EMITENTE.ie}</IE><CRT>1</CRT></emit><det nItem="1"><prod><cProd>001</cProd><cEAN>SEM GTIN</cEAN><xProd>TESTE</xProd><NCM>21069090</NCM><CFOP>5102</CFOP><uCom>UN</uCom><qCom>1.0000</qCom><vUnCom>10.00</vUnCom><vProd>10.00</vProd><cEANTrib>SEM GTIN</cEANTrib><uTrib>UN</uTrib><qTrib>1.0000</qTrib><vUnTrib>10.00</vUnTrib><indTot>1</indTot></prod><imposto><ICMS><ICMSSN102><orig>0</orig><CSOSN>102</CSOSN></ICMSSN102></ICMS><PIS><PISNT><CST>07</CST></PISNT></PIS><COFINS><COFINSNT><CST>07</CST></COFINSNT></COFINS></imposto></det><total><ICMSTot><vBC>0.00</vBC><vICMS>0.00</vICMS><vICMSDeson>0.00</vICMSDeson><vFCP>0.00</vFCP><vBCST>0.00</vBCST><vST>0.00</vST><vFCPST>0.00</vFCPST><vFCPSTRet>0.00</vFCPSTRet><vProd>10.00</vProd><vFrete>0.00</vFrete><vSeg>0.00</vSeg><vDesc>0.00</vDesc><vII>0.00</vII><vIPI>0.00</vIPI><vIPIDevol>0.00</vIPIDevol><vPIS>0.00</vPIS><vCOFINS>0.00</vCOFINS><vOutro>0.00</vOutro><vNF>10.00</vNF><vTotTrib>0.00</vTotTrib></ICMSTot></total><transp><modFrete>9</modFrete></transp><pag><detPag><tPag>01</tPag><vPag>10.00</vPag></detPag></pag><infRespTec><CNPJ>${EMITENTE.cnpj}</CNPJ><xContato>Elaine</xContato><email>a@a.com</email><fone>14999999999</fone></infRespTec></infNFe></NFe>`;
 
-        let xmlNFe = `<NFe xmlns="http://www.portalfiscal.inf.br/nfe">
-    <infNFe Id="NFe${chNFe}" versao="4.00">
-        <ide>
-            <cUF>${cUF}</cUF>
-            <cNF>${cNF}</cNF>
-            <natOp>VENDAS</natOp>
-            <mod>${mod}</mod>
-            <serie>1</serie>
-            <nNF>${nNF}</nNF>
-            <dhEmi>${dhEmiStr}</dhEmi>
-            <tpNF>1</tpNF>
-            <idDest>1</idDest>
-            <cMunFG>${EMITENTE.enderEmit.cMun}</cMunFG>
-            <tpImp>4</tpImp>
-            <tpEmis>${tpEmis}</tpEmis>
-            <cDV>${cDV}</cDV>
-            <tpAmb>${tpAmb}</tpAmb>
-            <finNFe>1</finNFe>
-            <indFinal>1</indFinal>
-            <indPres>1</indPres>
-            <procEmi>0</procEmi>
-            <verProc>1.0</verProc>
-        </ide>
-        <emit>
-            <CNPJ>${EMITENTE.cnpj}</CNPJ>
-            <xNome>${EMITENTE.xNome}</xNome>
-            <enderEmit>
-                <xLgr>${EMITENTE.enderEmit.xLgr}</xLgr>
-                <nro>${EMITENTE.enderEmit.nro}</nro>
-                <xBairro>${EMITENTE.enderEmit.xBairro}</xBairro>
-                <cMun>${EMITENTE.enderEmit.cMun}</cMun>
-                <xMun>${EMITENTE.enderEmit.xMun}</xMun>
-                <UF>${EMITENTE.enderEmit.uf}</UF>
-                <CEP>${EMITENTE.enderEmit.cep}</CEP>
-                <cPais>1058</cPais>
-                <xPais>BRASIL</xPais>
-            </enderEmit>
-            <IE>${EMITENTE.ie}</IE>
-            <CRT>1</CRT>
-        </emit>
-        <det nItem="1">
-            <prod>
-                <cProd>001</cProd>
-                <cEAN>SEM GTIN</cEAN>
-                <xProd>Produto de Teste Ibitinga</xProd>
-                <NCM>21069090</NCM>
-                <CFOP>5102</CFOP>
-                <uCom>UN</uCom>
-                <qCom>1.0000</qCom>
-                <vUnCom>150.00</vUnCom>
-                <vProd>150.00</vProd>
-                <cEANTrib>SEM GTIN</cEANTrib>
-                <uTrib>UN</uTrib>
-                <qTrib>1.0000</qTrib>
-                <vUnTrib>150.00</vUnTrib>
-                <indTot>1</indTot>
-            </prod>
-            <imposto>
-                <ICMS>
-                    <ICMSSN102>
-                        <orig>0</orig>
-                        <CSOSN>102</CSOSN>
-                    </ICMSSN102>
-                </ICMS>
-                <PIS>
-                    <PISNT>
-                        <CST>07</CST>
-                    </PISNT>
-                </PIS>
-                <COFINS>
-                    <COFINSNT>
-                        <CST>07</CST>
-                    </COFINSNT>
-                </COFINS>
-            </imposto>
-        </det>
-        <total>
-            <ICMSTot>
-                <vBC>0.00</vBC>
-                <vICMS>0.00</vICMS>
-                <vICMSDeson>0.00</vICMSDeson>
-                <vFCP>0.00</vFCP>
-                <vBCST>0.00</vBCST>
-                <vST>0.00</vST>
-                <vFCPST>0.00</vFCPST>
-                <vFCPSTRet>0.00</vFCPSTRet>
-                <vProd>150.00</vProd>
-                <vFrete>0.00</vFrete>
-                <vSeg>0.00</vSeg>
-                <vDesc>0.00</vDesc>
-                <vII>0.00</vII>
-                <vIPI>0.00</vIPI>
-                <vIPIDevol>0.00</vIPIDevol>
-                <vPIS>0.00</vPIS>
-                <vCOFINS>0.00</vCOFINS>
-                <vOutro>0.00</vOutro>
-                <vNF>150.00</vNF>
-                <vTotTrib>0.00</vTotTrib>
-            </ICMSTot>
-        </total>
-        <transp>
-            <modFrete>9</modFrete>
-        </transp>
-        <pag>
-            <detPag>
-                <tPag>01</tPag>
-                <vPag>150.00</vPag>
-            </detPag>
-        </pag>
-        <infRespTec>
-            <CNPJ>${EMITENTE.cnpj}</CNPJ>
-            <xContato>Elaine Cristina</xContato>
-            <email>suporte@ibitinga.com</email>
-            <fone>14999999999</fone>
-        </infRespTec>
-    </infNFe>
-</NFe>`;
+        const pfxData = fs.readFileSync(path.join(__dirname, 'certificado.pfx'));
+        const p12 = forge.pkcs12.pkcs12FromAsn1(forge.asn1.fromDer(pfxData.toString('binary')), process.env.CERT_PASSWORD);
+        const key = forge.pki.privateKeyToPem(p12.getBags({ bagType: forge.pki.oids.pkcs8ShroudedKeyBag }).pkcs8ShroudedKeyBag[0].key);
 
-        let xmlAssinado = xmlNFe;
-        const certPath = path.join(__dirname, 'certificado.pfx');
-        if (fs.existsSync(certPath)) {
-            const pfxData = fs.readFileSync(certPath);
-            const p12Asn1 = forge.asn1.fromDer(pfxData.toString('binary'));
-            const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, process.env.CERT_PASSWORD);
-            const keyBags = p12.getBags({ bagType: forge.pki.oids.pkcs8ShroudedKeyBag });
-            const pkcs8Bag = keyBags[forge.pki.oids.pkcs8ShroudedKeyBag][0];
-            const privateKey = forge.pki.privateKeyToPem(pkcs8Bag.key);
+        const sig = new SignedXml();
+        sig.addReference("//*[local-name()='infNFe']", ["http://www.w3.org/2000/09/xmldsig#enveloped-signature", "http://www.w3.org/2001/10/xml-exc-c14n#"], "http://www.w3.org/2001/04/xmlenc#sha256");
+        sig.signingKey = key;
+        sig.computeSignature(xmlNFe);
 
-            const sig = new SignedXml();
-            sig.addReference(
-                "//*[local-name()='infNFe']",
-                ["http://www.w3.org/2000/09/xmldsig#enveloped-signature", "http://www.w3.org/2001/10/xml-exc-c14n#"],
-                "http://www.w3.org/2001/04/xmlenc#sha256"
-            );
-            sig.signingKey = privateKey;
-            sig.computeSignature(xmlNFe);
-            xmlAssinado = sig.getSignedXml();
-        } else {
-            return res.status(500).json({ sucesso: false, erro: "Certificado digital não encontrado no servidor." });
-        }
-
-        res.json({
-            sucesso: true,
-            mensagem: "XML gerado e assinado digitalmente com sucesso!",
-            xmlAssinado,
-            chaveAcesso: chNFe
-        });
-
-    } catch (erro) {
-        console.error("Erro ao gerar/assinar XML:", erro);
-        res.status(500).json({ sucesso: false, erro: erro.message });
-    }
+        res.json({ sucesso: true, xmlAssinado: sig.getSignedXml() });
+    } catch (e) { res.status(500).json({ erro: e.message }); }
 });
 
-// Rota 2: Transmitir para a SEFAZ-SP
 app.post('/transmitir-nfce', async (req, res) => {
     try {
-        let { xmlAssinado } = req.body;
-
-        if (!xmlAssinado) {
-            return res.status(400).json({ sucesso: false, erro: "XML assinado não fornecido." });
-        }
-
-        xmlAssinado = xmlAssinado.replace(/>\s+</g, '><').trim();
-
-        const httpsAgent = new https.Agent({
-            pfx: fs.readFileSync(path.join(__dirname, 'certificado.pfx')),
-            passphrase: process.env.CERT_PASSWORD,
-            rejectUnauthorized: false,
-            secureProtocol: 'TLSv1_2_method'
+        const soap = `<soap12:Envelope xmlns:soap12="http://www.w3.org/2003/05/soap-envelope"><soap12:Body><nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NFeAutorizacao4"><enviNFe xmlns="http://www.portalfiscal.inf.br/nfe" versao="4.00"><idLote>1</idLote><indSinc>1</indSinc>${req.body.xmlAssinado}</enviNFe></nfeDadosMsg></soap12:Body></soap12:Envelope>`;
+        const resp = await axios.post("https://homologacao.nfce.fazenda.sp.gov.br/ws/NFeAutorizacao4.asmx", soap, {
+            headers: { 'Content-Type': 'application/soap+xml; charset=utf-8' },
+            httpsAgent: new https.Agent({ pfx: fs.readFileSync(path.join(__dirname, 'certificado.pfx')), passphrase: process.env.CERT_PASSWORD, rejectUnauthorized: false })
         });
-
-        const soapEnvelope = `<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-            <soap12:Body>
-                <nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NFeAutorizacao4">
-                    <enviNFe xmlns="http://www.portalfiscal.inf.br/nfe" versao="4.00">
-                        <idLote>1</idLote>
-                        <indSinc>1</indSinc>
-                        ${xmlAssinado}
-                    </enviNFe>
-                </nfeDadosMsg>
-            </soap12:Body>
-        </soap12:Envelope>`;
-
-        const isProd = process.env.AMBIENTE_PRODUCAO === 'true';
-        const urlSefaz = isProd 
-            ? "https://nfce.fazenda.sp.gov.br/ws/NFeAutorizacao4.asmx"
-            : "https://homologacao.nfce.fazenda.sp.gov.br/ws/NFeAutorizacao4.asmx";
-
-        const resposta = await axios.post(urlSefaz, soapEnvelope, {
-            headers: { 
-                'Content-Type': 'application/soap+xml; charset=utf-8',
-                'SOAPAction': 'http://www.portalfiscal.inf.br/nfe/wsdl/NFeAutorizacao4/nfeAutorizacaoLote'
-            },
-            httpsAgent,
-            timeout: 30000
-        });
-
-        const xmlRetorno = resposta.data;
-
-        const cStatMatch = xmlRetorno.match(/<cStat>(.*?)<\/cStat>/);
-        const xMotivoMatch = xmlRetorno.match(/<xMotivo>(.*?)<\/xMotivo>/);
-        
-        const cStat = cStatMatch ? cStatMatch[1] : "Desconhecido";
-        const xMotivo = xMotivoMatch ? xMotivoMatch[1] : "Retorno não processado corretamente";
-
-        res.json({
-            sucesso: cStat === "100" || cStat === "103" || cStat === "104",
-            cStat,
-            xMotivo,
-            retornoCompletoSefaz: xmlRetorno
-        });
-
-    } catch (erro) {
-        const detalheErro = erro.response ? erro.response.data : erro.message;
-        console.error("Erro crítico na transmissão SEFAZ:", detalheErro);
-        res.status(500).json({ sucesso: false, erro: detalheErro });
-    }
+        res.json({ xml: resp.data });
+    } catch (e) { res.status(500).json({ erro: e.response?.data || e.message }); }
 });
 
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+app.listen(process.env.PORT || 10000);
